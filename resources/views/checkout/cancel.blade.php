b6cda2665d70bfcbedcbe3f3a604445cae733742
vinothphpstormacc7 <vinothphpstormacc7@gmail.com>
2024-04-30 12:18:13 +0530

    update: added payment cancel page

---
+++ resources/views/checkout/cancel.blade.php
@@ -0,0 +1,17 @@
+<!-- success.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Payment Failure!</div>
+                    <div class="card-body">
+                        <p>Your current request was cancelled</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
